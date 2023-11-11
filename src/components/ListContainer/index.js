45f8961636cf514741ec92833bd2ebc067620b28
swathi <polojuswathi0202@gmail.com>
2023-11-11 13:05:04 +0000

    solution

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -7,10 +7,12 @@ class ListContainer extends Component {
   componentDidMount() {
     this.listBlogData()
   }
+
   listBlogData = async () => {
     const response = await fetch(
       'https://run.mocky.io/v3/77a7e71b-804a-4fbd-822c-3e365d3482cc',
     )
+
     const data = response.json()
 
     const updatedData = data.map(eachItem => ({
@@ -20,8 +22,10 @@ class ListContainer extends Component {
     }))
     this.setState({listData: updatedData})
   }
+
   render() {
     const {listData} = this.state
+
     return (
       <div>
         <h1>UNI Resto Cafe</h1>
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,4 +1,5 @@
 const ListItem = props => {
   const {listData} = props
+  return <h1>{listData}</h1>
 }
 export default ListItem
