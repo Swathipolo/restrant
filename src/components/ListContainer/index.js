7eac3340ff8d4509b59697404d56382146921951
swathi <polojuswathi0202@gmail.com>
2023-11-15 11:50:05 +0000

    solution

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -1,9 +1,9 @@
 import {Component} from 'react'
+
 import ListItem from '../ListItem'
-import ListDish from '../ListDish'
 
 class ListContainer extends Component {
-  state = {listData: [], listDish: []}
+  state = {listData: []}
 
   componentDidMount() {
     this.listBlogData()
@@ -17,51 +17,38 @@ class ListContainer extends Component {
     const data = await response.json()
 
     const updatedData = data.map(eachItem => ({
-      restaurantId: eachItem.restaurant_id,
       restaurantName: eachItem.restaurant_name,
       restaurantImage: eachItem.restaurant_image,
       tableMenuList: eachItem.table_menu_list,
       menuCategory: eachItem.menu_category,
-      menuCategoryId: eachItem.menu_category_id,
       menuCategoryImage: eachItem.menu_category_image,
       nextUrl: eachItem.nextUrl,
+      categoryDishes: eachItem.category_dishes,
+      dishId: eachItem.dish_id,
+      dishName: eachItem.dish_name,
+      dishPrice: eachItem.dish_price,
+      dishImage: eachItem.dish_image,
+      dishCurrency: eachItem.dish_currency,
+      dishCalories: eachItem.dish_calories,
+      dishDescription: eachItem.dish_description,
+      dishAvailability: eachItem.dish_Availability,
+      dishType: eachItem.dish_Type,
     }))
     this.setState({listData: updatedData})
-
-    const updatedDish = data.map(eachDish => ({
-      categoryDishes: eachDish.category_dishes,
-      dishId: eachDish.dish_id,
-      dishName: eachDish.dish_name,
-      dishPrice: eachDish.dish_price,
-      dishImage: eachDish.dish_image,
-      dishCurrency: eachDish.dish_currency,
-      dishCalories: eachDish.dish_calories,
-      dishDescription: eachDish.dish_description,
-      dishAvailability: eachDish.dish_Availability,
-      dishType: eachDish.dish_Type,
-    }))
-    this.setState({listDish: updatedDish})
   }
 
   render() {
-    const {listData, listDish} = this.state
+    const {listData} = this.state
 
     return (
       <div>
-        <h1>UNI Resto Cafe</h1>
-
         {listData.map(eachList => (
-          <ListItem listData={eachList} key={eachList.id} />
+          <ListItem
+            listData={eachList}
+            key={eachList.dishId}
+            name={eachList.dishName}
+          />
         ))}
-
-        <ul>
-          {listDish.map(eachDishItem => (
-            <ListDish
-              listDish={eachDishItem}
-              key={eachDishItem.categoryDishes}
-            />
-          ))}
-        </ul>
       </div>
     )
   }
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,12 +1,30 @@
 const ListItem = props => {
   const {listData} = props
-  const {restaurantName, restaurantImage} = listData
+  const {
+    restaurantName,
+    name,
+    dishPrice,
+    dishImage,
+    dishCurrency,
+    dishCalories,
+    dishDescription,
+    dishAvailability,
+    dishType,
+  } = listData
 
   return (
     <div>
       <h1>{restaurantName}</h1>
       <p>My Orders</p>
-      <img src={restaurantImage} alt={restaurantImage} />
+
+      <p>{name}</p>
+      <img src={dishImage} alt={dishImage} />
+      <p>{dishPrice}</p>
+      <p>{dishCurrency}</p>
+      <p>{dishCalories}</p>
+      <p>{dishDescription}</p>
+      <p>{dishAvailability}</p>
+      <p>{dishType}</p>
     </div>
   )
 }
